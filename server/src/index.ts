c80dc4d93dda3fced3aaf1c0f2fba20988522ddc
zendagin <zendagin1126@gmail.com>
2022-11-13 20:47:37 +0900

    feat: cors for local dev

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,12 @@ import express from "express";
 import {initOcrJobsCron} from "./api/ocr-jobs/ocr-jobs-cron";
 import routes from "./api/routes";
 import {initDatabase} from "./database/init";
+import cors from "cors";
 const app = express();
 const port = process.env.PORT || 3000;
+
+if (process.env.LOCAL)
+    app.use(cors());
 
 app.get("/", (req, res) => {
     res.status(200).json({success: true});
